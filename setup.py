850b5d52df91182a120f83898414a8613eec4568
AxelTLarsson <mail@axellarsson.nu>
2016-01-31 18:43:53 +0100

    Smarter name for console script entry

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='An AI playing the game of Reversi with Othello rules',
     long_description=long_description,
@@ -102,7 +102,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'othello=othello:main',
+            'main=othello:main',
         ],
     },
 )
