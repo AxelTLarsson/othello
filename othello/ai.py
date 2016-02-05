2340a6eadc4b577f3fae535c0435b920f8c6a567
lb08abu <belcher_lewis@yahoo.com>
2016-02-05 23:47:10 +0100

    Working minimax algorithm

--- othello/ai.py
+++ othello/ai.py
@@ -1,5 +1,7 @@
 
+import numpy as np
 from othello.game import Board, Player, Game
+from copy import deepcopy
 
 
 class WeightBoard(Board):
@@ -31,18 +33,90 @@ class AI:
         self._weight_board[0, -1] = corner_weight
         self._weight_board[-1, -1] = corner_weight
 
-        print(self._weight_board)
+        self._expanded_states = []
 
-    def find_best_move(self, game:Game, player):
-        best_moves = dict()
-        for move in game.legal_moves(player):
-            
+    def mini_max_decision(self, state):
+        moves = state.legal_moves()
+        scores = [self.min_value(self.result(state, a)) for a in moves]
+        return moves[np.argmax(scores)]
 
-    def move_score(self, state, move):
-        state[move] = int(self.player)
-        return sum(self._weight_board * state)
+    def max_value(self, state):
+        if state.is_terminal():
+            return self.utility(state)
+        v = -np.inf
+        for a in state.legal_moves():
+            v = max(v, self.min_value(self.result(state, a)))
+        return v
+
+    def min_value(self, state):
+        if state.is_terminal():
+            return self.utility(state)
+        v = np.inf
+        for a in state.legal_moves():
+            v = min(v, self.max_value(self.result(state, a)))
+        return v
+
+    def result(self, state, a):
+        state_copy = state.copy()
+        state_copy.move(a)
+        return state_copy
+
+    def alpha_beta_pruning(self):
+        pass
+
+    def utility(self, state):
+        # here we're using the weight board to gauge how well a player is doing
+        return (self._weight_board * state).sum()
+
+    def __str__(self):
+        return "AI class with %s expanded states" % self._expanded_states
+
+
+class TestGame:
+    """
+    A test game mirroring the example in the book to test the AI.
+    """
+
+    def __init__(self):
+        self.state = 'start'
+        self.states = {
+            'a1': {'b1':  3, 'b2': 12, 'b3': 8},
+            'a2': {'c1':  2, 'c2':  4, 'c3': 6},
+            'a3': {'d1': 14, 'd2':  5, 'd3': 2},
+        }
+        self.end_states = []
+        for s in self.states.values():
+            self.end_states.extend(list(s.keys()))
+
+    def move(self, place):
+        assert place in self.states.keys()
+        self.state = place
+        self.states = self.states[place]
+
+    def legal_moves(self):
+        try:
+            return list(self.states.keys())
+        except AttributeError:
+            return None
+
+    def is_terminal(self):
+        if self.state in self.end_states:
+            return True
+        else:
+            return False
+
+    def copy(self):
+        return deepcopy(self)
+
+    def __mul__(self, other):
+        return self.states * other
+
+    def __rmul__(self, other):
+        return self.states * other
 
 
 if __name__ == '__main__':
     players = [Player('black'), Player('white')]
     ai = AI(players[0], time_limit=10)
+    game = TestGame()
+    print(ai.mini_max_decision(game))
