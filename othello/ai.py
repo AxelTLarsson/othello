9427299b16c8dc3e8e0d16a4d9604a966739ac46
lb08abu <belcher_lewis@yahoo.com>
2016-02-09 16:18:43 +0100

    Updated test game and potentially working depth limited search

--- othello/ai.py
+++ othello/ai.py
@@ -20,7 +20,13 @@ class AI:
     """
     Base class for MiniMax and AlphaBeta AI agents.
     """
-    def __init__(self, player, time_limit, edge_weight=3, corner_weight=10):
+    def __init__(self, player, time_limit, edge_weight=3, corner_weight=10,
+                 depth=None):
+        if depth is None:
+            self.depth = np.inf
+        else:
+            self.depth = depth
+
         self.player = player
         self._time_limit = time_limit
         self._weight_board = WeightBoard() + 1
@@ -62,27 +68,31 @@ class AI:
 
 class MiniMaxAI(AI):
 
-    def search(self, state, depth=None):
+    def search(self, state):
         moves = state.legal_moves()
-        scores = [self.min_value(self.result(state, a)) for a in moves]
+        scores = [self.min_value(self.result(state, a), self.depth)
+                  for a in moves]
         print(max(scores))
         return moves[np.argmax(scores)]
 
-    def max_value(self, state):
-        if state.is_terminal():
+    def max_value(self, state, depth):
+        if self.cut_off(state, depth):
             return self.utility(state)
         v = -np.inf
         for a in state.legal_moves():
-            v = max(v, self.min_value(self.result(state, a)))
+            v = max(v, self.min_value(self.result(state, a), depth-1))
         return v
 
-    def min_value(self, state):
-        if state.is_terminal():
+    def min_value(self, state, depth):
+        if self.cut_off(state, depth):
             return self.utility(state)
         v = np.inf
         for a in state.legal_moves():
-            v = min(v, self.max_value(self.result(state, a)))
+            v = min(v, self.max_value(self.result(state, a), depth-1))
         return v
+
+    def cut_off(self, state, depth):
+        return state.is_terminal() or depth <= 0
 
 
 class AlphaBetaAI(AI):
@@ -121,23 +131,19 @@ class TestGame:
     """
     def __init__(self):
         self.state = 'start'
-        self.end_states = []
 
-        # dict version
-        self.states = OrderedDict()
-        states = {
-            'a1': {'b1':  3, 'b2': 12, 'b3': 8},
-            'a2': {'c1':  2, 'c2':  4, 'c3': 6},
-            'a3': {'d1': 14, 'd2':  5, 'd3': 2},
+        self.scores = {
+            'a1': 10, 'a2': 15, 'a3': 4,
+            'b1':  3, 'b2': 12, 'b3': 8,
+            'c1':  2, 'c2':  4, 'c3': 6,
+            'd1': 14, 'd2':  5, 'd3': 2,
         }
 
-        for k1 in sorted(states.keys()):
-            self.states[k1] = OrderedDict()
-            for k, v in sorted(states[k1].items(), key=lambda x: x[0]):
-                self.states[k1][k] = v
-
-        for s in states.values():
-            self.end_states.extend(list(s.keys()))
+        self.states = OrderedDict([
+            ('a1', OrderedDict([('b1', 'x'), ('b2', 'x'), ('b3', 'x')])),
+            ('a2', OrderedDict([('c1', 'x'), ('c2', 'x'), ('c3', 'x')])),
+            ('a3', OrderedDict([('d1', 'x'), ('d2', 'x'), ('d3', 'x')])),
+        ])
 
     def move(self, place):
         assert place in self.states.keys()
@@ -151,30 +157,40 @@ class TestGame:
             return None
 
     def is_terminal(self):
-        if self.state in self.end_states:
-            return True
-        else:
-            return False
+        return self.states is 'x'
 
     def copy(self):
         return deepcopy(self)
 
     def __mul__(self, other):
-        return self.states * other
+        return self.scores[self.state] * other
 
     def __rmul__(self, other):
-        return self.states * other
+        return self.scores[self.state] * other
 
 
 if __name__ == '__main__':
+
+    game = TestGame()
+    print(game.states)
+
+    print("\nMinimax")
     ai = MiniMaxAI(1, time_limit=10)
     game = TestGame()
     print(ai.search(game))
     print(ai)
 
+    print("\nAlpha-beta")
     ai = AlphaBetaAI(1, time_limit=10)
     game = TestGame()
     print(ai.search(game))
     # with the default setup this should have expanded two less nodes
     # than the standard mini-max search
+    print(ai)
+
+    print("\nDepth limited")
+    ai = MiniMaxAI(1, time_limit=10, depth=0)
+    game = TestGame()
+    print(ai.depth)
+    print(ai.search(game))
     print(ai)
